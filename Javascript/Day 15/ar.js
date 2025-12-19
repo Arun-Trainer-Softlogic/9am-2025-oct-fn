4bccaffbd0d76a36aa6d06a6c56aceee88b70c0e
arunsla <arunraj.softlogic@gmail.com>
2025-12-19 14:09:48 +0530

    day 15

---
+++ Javascript/Day 15/ar.js
@@ -0,0 +1,31 @@
+// const user = {
+//   name: "Arun",
+//   age: 76,
+// };
+
+// const jsonData = JSON.stringify(user);
+
+// console.log(jsonData);
+
+// const userObj = JSON.parse(jsonData);
+
+// console.log(userObj);
+
+
+
+// const productJSON = '[{ "id": 1, "name": "Laptop" },{ "id": 2, "name": "Mobile" }]'
+
+// const products = JSON.parse(productJSON)
+
+// console.log(products);
+
+
+// products.forEach(p => {
+//     console.log(p.name);
+// });
+
+
+
+
+
+
