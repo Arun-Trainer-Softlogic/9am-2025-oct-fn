eb38595a33a376a7047a41da2d4f8bcee091688a
arunsla <arunraj.softlogic@gmail.com>
2026-01-21 16:29:54 +0530

    update

---
+++ sql/Day 2.sql
@@ -0,0 +1,15 @@
+-- create database ar ;
+
+
+-- create table table_name (
+-- coloumn_name datatype constraint ,
+-- coloumn_name datatype constraint ,
+-- )
+
+use ar;
+
+create TABLE student (
+id INT,
+name varchar(50),
+age int
+)
---
+++ sql/Day1.sql
@@ -0,0 +1,30 @@
+-- create database school
+ use school;
+
+-- create table students (
+-- id int ,
+-- name varchar(50),
+-- age int ,
+-- course varchar(30 )
+
+-- )
+
+
+
+-- select * from students
+
+-- insert into table_name values (val1 , val2)
+
+
+-- INSERT INTO students VALUES (
+-- 1, 'Arun', 20, 'SQL'
+-- );
+
+-- insert into students (id, name)
+-- values (2,'Meenu');
+-- INSERT INTO students VALUES
+-- (3, 'Ravi', 22, 'Java'),
+-- (4, 'Anu', 21, 'Python'),
+-- (5, 'Kiran', 23, 'SQL');
+
+-- select * from students
